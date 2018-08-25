a9c2ac2400665dfb0e6241c9e581c9b564a0afff
harrytsz <18810271846@163.com>
2018-08-25 21:49:33 +0800

    Add Keyboard control ball move speed function

--- SourceCode/pygametest01.py
+++ SourceCode/pygametest01.py
@@ -15,6 +15,16 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+		elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                speed[0] = speed[0] if speed[0] == 0 else (abs(speed[0]) - 1)*int(speed[0]/abs(speed[0]))
+            elif event.key == pygame.K_RIGHT:
+                speed[0] = speed[0] + 1 if speed[0] > 0 else speed[0] - 1
+            elif event.key == pygame.K_UP:
+                speed[1] = speed[1] + 1 if speed[1] > 0 else speed[1] - 1
+            elif event.key == pygame.K_DOWN:
+                speed[1] = speed[1] if speed[1] == 0 else (abs(speed[1] - 1))*int(speed[1]/abs(speed[1]))
+    ballrect = ballrect.move(speed[0], speed[1])
     ballrect = ballrect.move(speed[0], speed[1])
     if ballrect.left < 0 or ballrect.right > width:
         speed[0] = - speed[0]
